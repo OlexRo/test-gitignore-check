8469f85e309a72c410b1afa09289e0d751b6ba65
Алексей <alexromanenco112003@gmail.com>
2025-11-23 15:45:22 +0800

    update_composer

--- backend/src/main/java/com/api/backend/service/impl/UserServiceImpl.java
+++ backend/src/main/java/com/api/backend/service/impl/UserServiceImpl.java
@@ -9,7 +9,6 @@ import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import java.util.List;
 import java.util.stream.Collectors;
 
